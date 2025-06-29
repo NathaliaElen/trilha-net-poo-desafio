50d9ca77eb666c31b600abef8e82b0f0006bda9b
Nathália Elen <nathaliaelenestudos@gmail.com>
2025-06-29 16:19:54 -0300

    feat: implementar classe Iphone com herança de Smartphone e sobrescrever o método InstalarAplicativo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} via App Store no iPhone.");
+            System.Threading.Thread.Sleep(2000); // Simula o tempo de instalação
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso.");
+        }
     }
 }
