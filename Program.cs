d2347fdb71f2a9a797d988bf5866d9a2cf52accc
Nathália Elen <nathaliaelenestudos@gmail.com>
2025-06-29 16:34:09 -0300

    chore: melhorar legibilidade passando parâmetros nomeados nos construtores no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
-Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 64);
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456789012345", memoria: 64);
 nokia.ExibirInformacoes();
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.Desligar();
 nokia.InstalarAplicativo("WhatsApp");
 
-Iphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+Iphone iphone = new Iphone(numero: "987654321", modelo: "IPhone 12", imei: "987654321098765", memoria: 128);
 iphone.ExibirInformacoes();
 iphone.Ligar();
 iphone.ReceberLigacao();
