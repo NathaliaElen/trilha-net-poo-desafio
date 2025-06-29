631c2ce60831a5dc00ffb743640299f802f1eb3a
Nathália Elen <nathaliaelenestudos@gmail.com>
2025-06-29 16:16:06 -0300

    feat: implementar classe Nokia com herança de Smartphone e sobrescrever o método InstalarAplicativo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} via Play Store no Nokia.");
+            System.Threading.Thread.Sleep(2000); // Simula o tempo de instalação
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso.");
+        }
     }
 }
